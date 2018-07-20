ed762c77019fbf79755cd67ab87d4e8c3a0fbaa5
dunnywei <wei_don@yahoo.com>
2018-07-20 17:16:28 -0400

    move interview_july_20_2018.c to interview repository

---
+++ interview_july_20_2018.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t getline(char **lineptr, size_t *n, FILE *stream);
+
+int main() {
+    size_t maxLineLen = 1024;
+    char *line = (char*)malloc(maxLineLen);
+    while(getline(&line, &maxLineLen, stdin) != -1) {
+        printf("%s", line);
+    }
+}
+
+
+size_t getline(char **lineptr, size_t *n, FILE *stream)
+{
+    int a=0;
+    int b=0;
+    int c=0;
+    printf("enter you number\n");
+    scanf("%d %d %d",&a,&b,&c);
+    printf("a is %d, b is %d c is %d \n",a,b,c); 
+    return -1;    
+}
+
+
+/*
+1)Calculate the integers and square of input
+2)Calculate values from N p q and replace it with desired output
+3)From input of 2014-02-07,3,monkey put them into
+different categories according to the date,total items in the same date
+and is it a unique or not
+input
+2014-02-07,3,monkey
+2014-02-07,2,bear
+2014-02-08,3,bear
+output
+2014-02-07,5,2
+2014-02-08,3,1
+*/
